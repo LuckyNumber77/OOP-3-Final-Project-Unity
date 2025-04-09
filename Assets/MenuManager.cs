2fa974785a4eb85966b7607ca4c702508c1fdec0
joel5959 <joel.williams@dcmail.ca>
2025-04-09 14:25:33 -0400

    Games Rules Scene
    Added a games rule scene (incomplete tho and the back button is not functioning yet)

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -9,17 +9,21 @@ public class MenuManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Game Play"); // Replace "Game Play" with your exact Gameplay scene name if different
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     // Called when Game Rules button is clicked
     public void ShowRules()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         rulesPanel.SetActive(true); // Show the Rules Panel
     }
 
     // Called when Close button on Rules Panel is clicked
     public void HideRules()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         rulesPanel.SetActive(false); // Hide the Rules Panel
     }
 }
